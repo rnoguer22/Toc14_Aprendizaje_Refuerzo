058a40c789398a7d2122be673fd68eb3ba6482a1
rnoguer22 <rnogugon@myuax.com>
2023-12-23 11:59:45 +0100

    actualizamos version de numpy para solucionar un error

--- refuerzo.py
+++ refuerzo.py
@@ -2,6 +2,36 @@ import gym
 import random
 import numpy as np
 
+# Initialize the non-slippery Frozen Lake environment
 environment = gym.make("FrozenLake-v1", is_slippery=False, render_mode="rgb_array")
 environment.reset()
 environment.render()
+
+# Initialize Q-table with zeros
+# Our table has the following dimensions:
+# (rows x columns) = (states x actions) = (16 x 4)
+qtable = np.zeros((16, 4))
+
+# Alternatively, the gym library can also directly
+# give us the number of states and actions using 
+# "env.observation_space.n" and "env.action_space.n"
+nb_states = environment.observation_space.n  # = 16
+nb_actions = environment.action_space.n      # = 4
+qtable = np.zeros((nb_states, nb_actions))
+
+# Let's see how it looks
+#print('Q-table =')
+#print(qtable)
+
+print(environment.action_space.sample())
+
+
+# 1. Randomly choose an action using action_space.sample()
+action = environment.action_space.sample()
+
+# 2. Implement this action and move the agent in the desired direction
+new_state, reward, done, truncated, info = environment.step(action)
+
+# Display the results (reward and map)
+environment.render()
+print(f'Reward = {reward}')
