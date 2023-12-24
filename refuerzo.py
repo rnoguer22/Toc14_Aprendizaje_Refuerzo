a5d8cac10b1a78f6446baa17fdd632643a0c9b77
rnoguer22 <rnogugon@myuax.com>
2023-12-24 18:56:58 +0100

    Seguimos solucionando errores del codigo

--- refuerzo.py
+++ refuerzo.py
@@ -1,6 +1,9 @@
 import gym
-import random
 import numpy as np
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+import time 
+
 
 # Initialize the non-slippery Frozen Lake environment
 environment = gym.make("FrozenLake-v1", is_slippery=False, render_mode="rgb_array")
@@ -23,8 +26,6 @@ qtable = np.zeros((nb_states, nb_actions))
 #print('Q-table =')
 #print(qtable)
 
-print(environment.action_space.sample())
-
 
 # 1. Randomly choose an action using action_space.sample()
 action = environment.action_space.sample()
@@ -35,3 +36,141 @@ new_state, reward, done, truncated, info = environment.step(action)
 # Display the results (reward and map)
 environment.render()
 print(f'Reward = {reward}')
+
+print('===========================================')
+
+
+
+plt.rcParams['figure.dpi'] = 300
+plt.rcParams.update({'font.size': 17})
+
+# We re-initialize the Q-table
+qtable = np.zeros((environment.observation_space.n, environment.action_space.n))
+
+# Hyperparameters
+episodes = 1000        # Total number of episodes
+alpha = 0.5            # Learning rate
+gamma = 0.9            # Discount factor
+
+# List of outcomes to plot
+outcomes = []
+
+print('Q-table before training:')
+print(qtable)
+
+# Training
+for _ in range(episodes):
+    state = environment.reset()
+    state = int(state[0])
+    done = False
+
+    # By default, we consider our outcome to be a failure
+    outcomes.append("Failure")
+
+    # Until the agent gets stuck in a hole or reaches the goal, keep training it
+    while not done:
+        # Choose the action with the highest value in the current state
+        if np.max(qtable[state]) > 0:
+          action = np.argmax(qtable[state])
+
+        # If there's no best action (only zeros), take a random one
+        else:
+          action = environment.action_space.sample()        
+    
+        # Implement this action and move the agent in the desired direction
+        new_state, reward, done, truncated, info = environment.step(action)
+
+
+        # Update Q(s,a)
+        qtable[state, action] = qtable[state, action] + \
+                                alpha * (reward + gamma * np.max(qtable[new_state]) - 
+                                qtable[state, action])
+             
+        # Update our current state
+        state = new_state
+
+        # If we have a reward, it means that our outcome is a success
+        if reward:
+          outcomes[-1] = "Success"
+
+print()
+print('===========================================')
+print('Q-table after training:')
+print(qtable)
+
+# Plot outcomes
+plt.figure(figsize=(12, 5))
+plt.xlabel("Run number")
+plt.ylabel("Outcome")
+ax = plt.gca()
+plt.bar(range(len(outcomes)), outcomes, width=1.0)
+plt.show()
+
+
+
+episodes = 100
+nb_success = 0
+
+# Evaluation
+for _ in range(episodes):
+    state = environment.reset()
+    state = int(state[0])
+    done = False
+    
+    # Until the agent gets stuck or reaches the goal, keep training it
+    while not done:
+        # Choose the action with the highest value in the current state
+        if np.max(qtable[state]) > 0:
+          action = np.argmax(qtable[state])
+
+        # If there's no best action (only zeros), take a random one
+        else:
+          action = environment.action_space.sample()
+          
+        # Implement this action and move the agent in the desired direction
+        new_state, reward, done, truncated, info = environment.step(action)
+       
+        # Update our current state
+        state = new_state
+
+        # When we get a reward, it means we solved the game
+        nb_success += reward
+
+# Let's check our success rate!
+print (f"Success rate = {nb_success/episodes*100}%")
+
+
+
+print('===========================================')
+
+
+
+state = environment.reset()
+state = int(state[0])
+done = False
+sequence = []
+
+while not done:
+    # Choose the action with the highest value in the current state
+    if np.max(qtable[state]) > 0:
+      action = np.argmax(qtable[state])
+
+    # If there's no best action (only zeros), take a random one
+    else:
+      action = environment.action_space.sample()
+      
+    # Add the action to the sequence
+    sequence.append(action)
+
+    # Implement this action and move the agent in the desired direction
+    new_state, reward, done, truncated, info = environment.step(action)
+
+    # Update our current state
+    state = new_state
+
+    # Update the render
+    clear_output(wait=True)
+    environment.render()
+    time.sleep(1)
+
+print(f"Sequence = {sequence}")
